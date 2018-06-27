040ffbfde5f46d2c131c5a2616068c1f35637622
NadiaQN <quezada.nadia@gmail.com>
2018-06-27 19:43:39 -0400

    Validadando inputs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 /* Holiii acá va tu código también */
 
 let email = document.getElementById("exampleInputEmail1");
-let password = document.getElementById("exampleInputPassword").value
+let password = document.getElementById("exampleInputPassword")
 let btn1 = document.getElementById("btnValidar");
+const boxTwo = document.getElementById("caja-2");
+const boxOne = document.getElementById("caja-1");
 
 
-/* Aqui esta la validacion del input con el evento tecla */
+/* Aqui esta la validacion del input */
 btn1.addEventListener('click', () => {
-  if (email.value = '') {
-    alert('Debe introducir su correo electronico');
+  if (email.value == '' || password.value == '') {
+  alert('Debe introducir su correo electronico o contraseña'); 
   }
-})
-  
-/* aqui validacion del password */
-btn1.addEventListener('click', () => {
-  for ( var i = 0; password.length < i; i++)
-  if ( password[i] === number && password[i] === validarPassword); {
+  if (password.length == number && password.length <= 8) {
+  alert('POr favor introduzca solo 8 digitos numericos');
+  }
 
-  }
-  return 
+  // tratando que desasparezca //
+boxTwo.style.display = 'block';
+boxOne.style.display = 'none';
 })
+

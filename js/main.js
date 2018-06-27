db8c77915e9e90ba150d975b1527ec4a323ae9f6
NadiaQN <quezada.nadia@gmail.com>
2018-06-27 16:37:18 -0400

    primera section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 /* Holiii acá va tu código también */
+
+let email = document.getElementById("exampleInputEmail1");
+let password = document.getElementById("exampleInputPassword").value
+let btn1 = document.getElementById("btnValidar");
+
+
+/* Aqui esta la validacion del input con el evento tecla */
+btn1.addEventListener('click', () => {
+  if (email.value = '') {
+    alert('Debe introducir su correo electronico');
+  }
+})
+  
+/* aqui validacion del password */
+btn1.addEventListener('click', () => {
+  for ( var i = 0; password.length < i; i++)
+  if ( password[i] === number && password[i] === validarPassword); {
+
+  }
+  return 
+})
